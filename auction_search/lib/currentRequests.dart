3a2bba5941160d062395683c2c6807927bfbf47e
mateuszFicek <mateuszficek295@gmail.com>
2020-01-17 23:19:16 +0100

    Final commit
    Final update with everything working.

--- auction_search/lib/currentRequests.dart
+++ auction_search/lib/currentRequests.dart
@@ -1,3 +1,28 @@
+/*Copyright (c) 2020, Michał Kilian, Mateusz Gujda, Mateusz Ficek
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 import 'dart:developer';
 import 'dart:io';
 import 'package:auction_search/addNewRequestPage.dart';
@@ -30,6 +55,7 @@ Future clearUser() async {
   return true;
 }
 
+/// Funkcja, która wysyła zapytanie http, które usuwa Requesty.
 Future deleteRequest(int requestId) async {
   SharedPreferences user = await SharedPreferences.getInstance();
   String username = user.getString('username');
@@ -41,6 +67,7 @@ Future deleteRequest(int requestId) async {
       headers: {HttpHeaders.authorizationHeader: basicAuth});
 }
 
+/// Funkcja, która wysyła zapytanie http, które zmienia status Requestu.
 Future changeRequestStatus(int requestId, String newStatus) async {
   SharedPreferences user = await SharedPreferences.getInstance();
   String username = user.getString('username');
@@ -239,7 +266,7 @@ class _CurrentRequestsState extends State<CurrentRequests> {
     }
   }
 
-  /// Funkcja, która pobiera wszystkie zapytania, które użytkownik dodał od aplikacji.
+  /// Funkcja, która pobiera wszystkie aktywne zapytania, które użytkownik dodał od aplikacji.
   /// Zwraca listę zapytań.
   Future<List<Request>> fetchRequestsActive() async {
     List<Request> requests = [];
@@ -264,7 +291,7 @@ class _CurrentRequestsState extends State<CurrentRequests> {
     }
   }
 
-  /// Funkcja, która pobiera wszystkie zapytania, które użytkownik dodał od aplikacji.
+  /// Funkcja, która pobiera wszystkie zakończone oraz przerwane zapytania, które użytkownik dodał od aplikacji.
   /// Zwraca listę zapytań.
   Future<List<Request>> fetchRequestsClosed() async {
     List<Request> requests = [];
--- auction_search/lib/registerPage.dart
+++ auction_search/lib/registerPage.dart
@@ -1,3 +1,28 @@
+/*Copyright (c) 2020, Michał Kilian, Mateusz Gujda, Mateusz Ficek
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 import 'dart:convert';
 import 'dart:io';
 
@@ -10,7 +35,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
-/// Strona do rejestracji użytkownika/
+/// Strona do rejestracji użytkownika.
 /// Zawiera cztery pola: login, email, hasło oraz powtórzone hasło.
 /// Pozwala na rejestrację użytkownika oraz daje mu dostęp do głównej części aplikacji.
 class RegisterPage extends StatefulWidget {
@@ -40,6 +65,8 @@ class _RegisterPageState extends State<RegisterPage> {
 
   bool validatedData;
 
+  /// Funkcja, która wysyła zapytanie http na serwer z prośbą o rejestrację użytkownika.
+  /// Jeśli zwrócony zostanie kod 200 to użytkownik został poprawnie zarejestrowany i otrzyma możliwość logowania się.
   Future registerNewUser(String username, String password, String passwordAgain,
       String email) async {
     SharedPreferences user = await SharedPreferences.getInstance();
@@ -78,6 +105,7 @@ class _RegisterPageState extends State<RegisterPage> {
     }
   }
 
+  /// Formularz, który pobiera od użytkownika login.
   Form buildLoginTextField() {
     return Form(
       key: _formKeyLogin,
@@ -114,6 +142,7 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  /// Formularz, który pobiera od użytkownika email oraz sprawdza jego poprawność.
   Form buildEmailTextField() {
     return Form(
       key: _formKeyEmail,
@@ -155,6 +184,7 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  /// Formularz, który pobiera od użytkownika hasło.
   Form buildPasswordTextField() {
     return Form(
       key: _formKeyPassword,
@@ -192,6 +222,7 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
+  /// Formularz, który pobiera od użytkownika hasło ponownie.
   Form buildPasswordAgainTextField() {
     return Form(
       key: _formKeyPasswordAgain,
